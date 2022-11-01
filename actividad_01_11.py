b75e41f618beda5ea27d842c17219cf8073435cc
JuanJoseRodriguezF <115745541+JuanJoseRodriguezF@users.noreply.github.com>
2022-11-01 14:03:07 -0500

    Add files via upload
    Actividad arrays en clase 01_11

---
+++ actividad_01_11.py
@@ -0,0 +1,16 @@
+lugares = ['Berlín','París','New York','Madagascar','Alaska']
+print(lugares)#se imprime la lista original
+x=sorted(lugares)
+print(x)#se imprime la lista en orden alfabético
+print(lugares)#se imprime la lista original
+y=sorted(lugares, reverse=True)
+print(y)#se imprime la lista en orden alfabético inverso
+print(lugares)#se imprime la lista original
+lugares.reverse()
+print(lugares)#se imprime la lista original invertida
+lugares.reverse()
+print(lugares)#se imprime la lista original
+lugares.sort()
+print(lugares)#se imprime la lista en orden afabético
+lugares.sort(reverse=True)
+print(lugares)#se imprime la lista en orden alfabético inverso
